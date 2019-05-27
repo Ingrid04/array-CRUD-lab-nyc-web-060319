e2accc7ceda69949323fd72212e5526e15610ed3
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-05-27 23:56:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,42 @@
 def create_an_empty_array
-  
+   [ ]
 end
 
 def create_an_array
-  
+   dogs = ["puppy1", "puppy2"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+   dogs = ["puppy1", "puppy2"]
+   dogs.push("puppy3")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+    dogs = ["puppy1", "puppy2"]
+    dogs.unshift("puppy0")
 end
 
 def remove_element_from_end_of_array(array)
-  
+    dogs = ["puppy1", "puppy2"]
+    dogs.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+    dogs = ["puppy1", "puppy2"]
+    dogs.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+    dogs = ["puppy1", "puppy2"]
+    dogs[1]
 end
 
 def retrieve_first_element_from_array(array)
-  
+    dogs = ["puppy1", "puppy2"]
+    dogs.first
 end
 
 def retrieve_last_element_from_array(array)
-  
+    dogs = ["puppy1", "puppy2"]
+    dogs.last
 end
