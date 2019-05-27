889d432bb56852f669e014c0afca34c8d5cea428
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-05-27 23:58:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,12 +7,12 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-   dogs = ["puppy1", "puppy2"]
+   dogs = ["puppy1", "puppy2", "puppy3", "puppy4"]
    dogs.push("puppy5")
 end
 
 def add_element_to_start_of_array(array, element)
-    dogs = ["puppy1", "puppy2"]
+    dogs = ["puppy1", "puppy2", "puppy3", "puppy4"]
     dogs.unshift("puppy0")
 end
 
