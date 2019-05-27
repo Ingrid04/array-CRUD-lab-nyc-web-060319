ad5221e2a2f752b561f236930d69e129385f99a6
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-05-27 23:58:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,12 +3,12 @@ def create_an_empty_array
 end
 
 def create_an_array
-   dogs = ["puppy1", "puppy2"]
+   dogs = ["puppy1", "puppy2", "puppy3", "puppy4"]
 end
 
 def add_element_to_end_of_array(array, element)
    dogs = ["puppy1", "puppy2"]
-   dogs.push("puppy3")
+   dogs.push("puppy5")
 end
 
 def add_element_to_start_of_array(array, element)
@@ -17,26 +17,26 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-    dogs = ["puppy1", "puppy2"]
+    dogs = ["puppy1", "puppy2", "puppy3", "puppy4"]
     dogs.pop
 end
 
 def remove_element_from_start_of_array(array)
-    dogs = ["puppy1", "puppy2"]
+    dogs = ["puppy1", "puppy2", "puppy3", "puppy4"]
     dogs.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-    dogs = ["puppy1", "puppy2"]
+    dogs = ["puppy1", "puppy2", "puppy3", "puppy4"]
     dogs[1]
 end
 
 def retrieve_first_element_from_array(array)
-    dogs = ["puppy1", "puppy2"]
+    dogs = ["puppy1", "puppy2", "puppy3", "puppy4"]
     dogs.first
 end
 
 def retrieve_last_element_from_array(array)
-    dogs = ["puppy1", "puppy2"]
+    dogs = ["puppy1", "puppy2", "puppy3", "puppy4"]
     dogs.last
 end
