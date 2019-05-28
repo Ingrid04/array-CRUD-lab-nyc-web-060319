413d38bc2bb0cad22abc6b6878e786b3421c0a0a
Ingrid Fuentes <ingridfmelgar@gmail.com>
2019-05-28 00:07:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,41 +3,41 @@ def create_an_empty_array
 end
 
 def create_an_array
-   array = ["Wow", "I", "am", "really". "learning"]
+   array = ["Wow", "I", "am", "really", "learning"]
 end
 
 def add_element_to_end_of_array(array, element)
-   array = ["Wow", "I", "am", "really". "learning"]
+   array = ["Wow", "I", "am", "really", "learning"]
    element = "arrays!"
    array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-    array = ["Wow", "I", "am", "really". "learning"]
+    array = ["Wow", "I", "am", "really", "learning"]
     array.unshift("Wow")
 end
 
 def remove_element_from_end_of_array(array)
-    array = ["Wow", "I", "am", "really". "learning"]
+    array = ["Wow", "I", "am", "really", "learning"]
     array.pop
 end
 
 def remove_element_from_start_of_array(array)
-    array = ["Wow", "I", "am", "really". "learning"]
+    array = ["Wow", "I", "am", "really", "learning"]
     array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-    array = ["Wow", "I", "am", "really". "learning"]
+    array = ["Wow", "I", "am", "really", "learning"]
     array[1]
 end
 
 def retrieve_first_element_from_array(array)
-    array = ["Wow", "I", "am", "really". "learning"]
+    array = ["Wow", "I", "am", "really", "learning"]
     array.first
 end
 
 def retrieve_last_element_from_array(array)
-    array = ["Wow", "I", "am", "really". "learning"]
+    array = ["Wow", "I", "am", "really", "learning"]
     array.last
 end
